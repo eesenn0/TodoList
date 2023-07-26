1df1e691c2aef202b9254e4348ac0a6858644153
eesenn0 <emreesen7@gmail.com>
2023-07-26 08:38:30 +0300

    first commit

---
+++ myfirstwebapp/src/main/java/com/eesenn0/springboot/myfirstwebapp/todo/TodoService.java
@@ -0,0 +1,60 @@
+package com.eesenn0.springboot.myfirstwebapp.todo;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+import org.springframework.stereotype.Service;
+
+import jakarta.validation.Valid;
+
+@Service
+public class TodoService {
+
+	private static List<Todo> todos = new ArrayList<>();
+	
+	private static int todosCount = 0;
+
+	static {
+		todos.add(new Todo(++todosCount, "eesenn0", "Learn Spring", 
+							LocalDate.now().plusYears(1), false));
+		todos.add(new Todo(++todosCount, "eesenn0", "Learn SpringBoot", 
+							LocalDate.now().plusYears(2), false));
+		todos.add(new Todo(++todosCount, "eesenn0", "Learn MySQL", 
+							LocalDate.now().plusYears(3), false));
+	}
+
+	public List<Todo> findByUsername(String username) {
+		Predicate<? super Todo> predicate 
+		= todo -> todo.getUsername().equalsIgnoreCase(username);
+		
+		return todos.stream()
+				.filter(predicate)
+				.toList();
+	}
+	
+	public void addTodo(String username, String description, LocalDate targetDate, boolean done) {
+		Todo todo = new Todo(++todosCount, username, description, targetDate, done);
+		todos.add(todo);
+	}
+
+	public void deleteById(int id) {
+		Predicate<? super Todo> predicate 
+			= todo -> todo.getId() == id;
+		todos.removeIf(predicate);
+	}
+
+	public Todo findById(int id) {
+		Predicate<? super Todo> predicate 
+		= todo -> todo.getId() == id;
+		Todo todo = todos.stream().filter(predicate).findFirst().get();
+		
+		return todo;
+	}
+
+	public void updateTodo(@Valid Todo todo) {
+		deleteById(todo.getId());
+		todos.add(todo); 
+	}
+}
---
+++ myfirstwebapp/src/main/resources/data.sql
@@ -0,0 +1,12 @@
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10001, 'eesenn0', 'Get Spring Boot', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10002, 'eesenn0', 'Get Backendd', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10003, 'eesenn0', 'Get FullStackk', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10004, 'eesenn0', 'Get Spring Job', CURRENT_DATE(), false);
+
